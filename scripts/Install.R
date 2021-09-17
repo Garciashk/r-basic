c24b49cf6c7dbd49d8aaa061fd07326d6fa63b4a
Garciashk <67179308+Garciashk@users.noreply.github.com>
2021-09-17 16:46:54 -0500

    Create Install.R

---
+++ scripts/Install.R
@@ -0,0 +1,65 @@
+install.packages("tidyverse", dep = TRUE)
+
+library(tidyverse)
+
+install.packages("magic", dep = TRUE)
+
+library(magic)
+
+#Cuadrado Mágic
+magic(6)
+
+2^(-4)
+
+725%/%7
+
+725%%7
+
+725/7
+
+pi
+
+2*pi
+
+3^pi
+
+pi^2
+
+sqrt(200)
+
+exp(1)
+
+log(pi)
+
+log(32, 2)
+
+abs(-pi)
+
+factorial(4)
+
+choose(5, 3)
+
+6^log(4,6)
+
+log(4^6, 4)
+
+choose(3,5)
+
+sin(60*pi/180)
+
+cos(60*pi/180)
+
+sinpi(1/2) # = sin (pi/2) 
+
+tan(pi) # -1.224647e-16 ~ 0
+
+tan(pi/2) # 1.633124e+16 ~ Inf
+
+asin(0.8660254) # arc sin en radianes
+
+asin(0.8660254) * 180/pi # arc sin en grados
+
+
+
+
+
